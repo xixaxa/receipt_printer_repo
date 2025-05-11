b7525a2ba5e70fb24f209a6fa4b309fbb22612be
xixaxa <95424525+xixaxa@users.noreply.github.com>
2025-05-11 23:56:24 +0300

    Update run.sh

--- receipt_escpos_printer/run.sh
+++ receipt_escpos_printer/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/with-contenv bashio
-export PRINTER_IP=$(bashio::config 'printer_ip')
-export PRINTER_PORT=$(bashio::config 'printer_port')
+
+PRINTER_IP=$(bashio::config 'printer_ip')
+PRINTER_PORT=$(bashio::config 'printer_port')
+HA_URL=$(bashio::config 'ha_url')
+HA_TOKEN=$(bashio::config 'ha_token')
+
+export PRINTER_IP
+export PRINTER_PORT
+export HA_URL
+export HA_TOKEN
 
 python3 /print_list.py
